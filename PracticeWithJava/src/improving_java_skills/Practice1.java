a46c0234cc312a04f673140fff0a3307755787d8
harika penukonda <penukondaharika@gmail.com>
2020-02-04 00:01:05 -0500

    Practice1

--- PracticeWithJava/src/improving_java_skills/Practice1.java
+++ PracticeWithJava/src/improving_java_skills/Practice1.java
@@ -4,6 +4,15 @@ public class Practice1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int i; 
+		int sum=0;
+		for (i=1;i<=5;i++)
+		{
+			sum++;
+		}
+		
+		System.out.println(sum);
 
 	}
 
