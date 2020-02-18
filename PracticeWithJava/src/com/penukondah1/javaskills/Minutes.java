85c5d0e2eba11f04f3e60cb76d9dea8d7780a7be
harika penukonda <penukondaharika@gmail.com>
2020-02-18 15:53:05 -0500

    Minutes in a day

---
+++ PracticeWithJava/src/com/penukondah1/javaskills/Minutes.java
@@ -0,0 +1,19 @@
+package com.penukondah1.javaskills;
+
+public class Minutes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int min, hours, cal;
+		
+		min = 60;
+		hours = 24;
+		
+		cal = min * hours;
+		
+		System.out.print("The total number of minutes in a day is : " + cal );
+
+	}
+
+}
