8e00c9b49721bf222b7ab758e6981110a06ea9e7
harika penukonda <penukondaharika@gmail.com>
2020-02-18 17:59:35 -0500

    Multiplication of 5

---
+++ PracticeWithJava/src/com/penukondah1/udemy/Practice2.java
@@ -0,0 +1,29 @@
+package com.penukondah1.udemy;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+		System.out.println("5 * 3");
+		
+		System.out.println(5 * 3);
+		
+		int mul;
+		
+		for (int i=0 ; i <=10 ; i++) {
+			
+			mul = 5 * i;
+			
+			System.out.println("5 * " + i + " = " + mul);
+			
+			
+		}
+		
+		System.out.println("Number of seconds in a day " + 60 * 60 * 24);
+
+	}
+
+}
