c83e70aac3f3a29ddcb85b0db564067460dba89e
harika penukonda <penukondaharika@gmail.com>
2020-02-18 16:20:26 -0500

    Update HelloWorld1.java

--- PracticeWithJava/src/com/penukondah1/javaskills/HelloWorld1.java
+++ PracticeWithJava/src/com/penukondah1/javaskills/HelloWorld1.java
@@ -1,5 +1,22 @@
 package com.penukondah1.javaskills;
 
+
+//public keyword is an access modifier which represents visibility, it means it is visible to all.
+
+//class keyword is used to declare a class in java.
+
+//static is a keyword, if we declare any method as static, it is known as static method.
+//The core advantage of static method is that there is no need to create object to invoke the static method. 
+
+//void is the return type of the method, it means it doesn't return any value.
+
+//String[]args is used for command line argument
+
+//main represents startup of the program
+
+//System.out.println() is used to print statement.
+
+
 public class HelloWorld1 {
 	
 	public void test() {
