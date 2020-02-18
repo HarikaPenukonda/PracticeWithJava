903a0efb22a2589c414a57baa10dc5e40c41ce7f
harika penukonda <penukondaharika@gmail.com>
2020-02-18 15:52:08 -0500

    FloatConcept

---
+++ PracticeWithJava/src/com/penukondah1/javaskills/FloatConcept.java
@@ -0,0 +1,21 @@
+package com.penukondah1.javaskills;
+
+public class FloatConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float a , b, c;
+		
+		a = 5;
+		
+		b = 2;
+		
+		c = a / b;
+		
+		System.out.print(c);
+		
+
+	}
+
+}
