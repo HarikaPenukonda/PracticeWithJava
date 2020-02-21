00a71a2f17515ce9c5fd97d72c5cf0424ddd5d03
harika penukonda <penukondaharika@gmail.com>
2020-02-21 16:48:53 -0500

    Create DataTypes.java

---
+++ PracticeWithJava/src/com/penukondah1/javaskills/DataTypes.java
@@ -0,0 +1,27 @@
+package com.penukondah1.javaskills;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte b = 10;
+		short s = 20;
+		int i = 30; 
+		long l = 401;
+		float f = 50f;
+		char c = 'A';
+		double d = 60d;
+		boolean boo = true;
+		String str = "Hello";
+		
+		System.out.println(b + " " + s + " " + i + " " + l + " " + f + " " + c + " " + d +" " + boo + " " + str);
+		
+		
+		
+		
+		
+
+	}
+
+}
