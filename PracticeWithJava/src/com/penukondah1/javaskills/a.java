e8e99a2f90d2c868bb7d9c0304bdb81e68bc8016
harika penukonda <penukondaharika@gmail.com>
2020-03-21 20:21:18 -0400

    Create a.java

---
+++ PracticeWithJava/src/com/penukondah1/javaskills/a.java
@@ -0,0 +1,16 @@
+package com.penukondah1.javaskills;
+
+public class a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 10;
+		
+		x = 5;
+		
+		System.out.print(x);
+
+	}
+
+}
