dc2e8667e21ef7acff092125dcd48d0033ae9732
harika penukonda <penukondaharika@gmail.com>
2020-04-09 04:15:06 -0400

    Create Challenge4.java

---
+++ PracticeWithJava/src/com/penukondah1/javaskills/Challenge4.java
@@ -0,0 +1,22 @@
+package com.penukondah1.javaskills;
+
+import java.util.Scanner;
+
+public class Challenge4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 Scanner scan = new Scanner(System.in);
+	        int i = scan.nextInt();
+	        double d = scan.nextDouble();
+	        String s = new String();
+	        // Write your code here.
+
+	        System.out.println("String: " + s);
+	        System.out.println("Double: " + d);
+	        System.out.println("Int: " + i);
+
+	}
+
+}
