5a9e3a050feeaeefa117d14c2af6e1765a4be01d
harika penukonda <penukondaharika@gmail.com>
2020-05-16 20:10:25 -0400

    Create AcceptingUserInput.java

---
+++ PracticeWithJava/src/com/penukondah1/udemy/AcceptingUserInput.java
@@ -0,0 +1,18 @@
+package com.penukondah1.udemy;
+
+import java.util.Scanner;
+
+public class AcceptingUserInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int x;
+		System.out.println("Enter the value");
+		x = sc.nextInt();
+		System.out.println("The value of x is "+x);
+
+	}
+
+}
