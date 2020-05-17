1fb152272da6a9edf3a401d2247fb13024881226
harika penukonda <penukondaharika@gmail.com>
2020-05-16 20:21:54 -0400

    Create Practice1.java

---
+++ PracticeWithJava/src/com/penukondah1/udemy/Practice1.java
@@ -0,0 +1,19 @@
+package com.penukondah1.udemy;
+
+import java.util.Scanner;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name");
+		name = sc.next();
+		System.out.println("The name you entered is "+name);
+
+	}
+
+}
