8dd8dd85145ae587a8cb7ff89fedcedb53035e1b
harika penukonda <penukondaharika@gmail.com>
2020-05-25 22:46:09 -0400

    Create Case5.java

---
+++ PracticeWithJava/src/com/penukondah1/exceptionHandling/Case5.java
@@ -0,0 +1,16 @@
+package com.penukondah1.exceptionHandling;
+
+public class Case5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(10/0);
+
+	}
+
+}
+
+/* output: Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at com.penukondah1.exceptionHandling.Case5.main(Case5.java:8)
+ */
