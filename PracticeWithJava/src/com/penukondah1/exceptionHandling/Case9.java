2dc6aa2893b25c8631e0f0e6cb19ac9013bd4830
harika penukonda <penukondaharika@gmail.com>
2020-05-25 22:45:59 -0400

    Create Case9.java

---
+++ PracticeWithJava/src/com/penukondah1/exceptionHandling/Case9.java
@@ -0,0 +1,12 @@
+package com.penukondah1.exceptionHandling;
+
+public class Case9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		throw new ArithmeticException ("/ by zero");
+		//System.out.println("Hello"); // CE : unreachable code
+	}
+
+}
