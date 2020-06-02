bbccc0efd948d263bdf5ec94cbfc991e42534550
harika penukonda <penukondaharika@gmail.com>
2020-06-02 19:20:23 -0400

    Create A.java

---
+++ PracticeWithJava/src/com/penukondah1/MultiThreading/A.java
@@ -0,0 +1,25 @@
+package com.penukondah1.MultiThreading;
+
+public class A {
+	
+	public synchronized void d1 (B b)
+	{
+		System.out.println("Thread 1 starting to execute d1(B b)method"); 
+		try
+		{
+			Thread.sleep(5000);
+		}
+		catch(InterruptedException e)
+		{
+			
+		}
+		System.out.println("Thread 1 trying to call Class B last method");
+		b.last();
+	}
+	
+	public synchronized void last()
+	{
+		System.out.println("Inside A, this is last() method");
+	}
+
+}
