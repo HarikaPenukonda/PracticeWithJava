a0615875b1013c7b387c4b73a2acfce667b2c671
harika penukonda <penukondaharika@gmail.com>
2020-06-23 14:39:59 -0500

    Create TreeSetDemo.java

---
+++ PracticeWithJava/src/com/penukondah1/Collections/TreeSetDemo.java
@@ -0,0 +1,23 @@
+package com.penukondah1.Collections;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TreeSet t = new TreeSet();
+		t.add("A");
+		t.add("a");
+		t.add("B");
+		t.add("M");
+		t.add("Z");
+		//System.out.println(t.add(new Integer(10))); // ClassCastException as it does not allow Heterogenous objects
+		//System.out.println(t.add(null)); // NullPointerException
+		System.out.println(t); // [A, B, M, Z, a]
+		
+
+	}
+
+}
