3818ae1b7103e693777e79ab31e2df0935fcb9c7
harika penukonda <penukondaharika@gmail.com>
2020-06-25 14:47:42 -0500

    Update MyComparator1.java

--- PracticeWithJava/src/com/penukondah1/Collections/MyComparator1.java
+++ PracticeWithJava/src/com/penukondah1/Collections/MyComparator1.java
@@ -6,7 +6,8 @@ public class MyComparator1 implements Comparator {
 	
 	public int compare(Object obj1, Object obj2)
 	{
-		String s1 = (String)obj1; //typecasting
+		//String s1 = (String)obj1; //typecasting
+		String s1 = obj1.toString();
 		String s2 = obj2.toString();	
 		return s2.compareTo(s1);
 		//return -s1.compareTo(s2);
