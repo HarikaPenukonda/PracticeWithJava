b8eafe7009ebc12527347ea1c0824403c3427947
harika penukonda <penukondaharika@gmail.com>
2020-06-25 10:15:37 -0500

    Create SumOfTwoNumbers.java

---
+++ PracticeWithJava/src/com/penukondah1/Coding/SumOfTwoNumbers.java
@@ -0,0 +1,19 @@
+package com.penukondah1.Coding;
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum;
+		
+		int num1 = 1234567899;
+		int num2 = 1234567899;
+		
+		sum = num1 + num2;
+		System.out.println(sum); // -1825831498
+		
+
+	}
+
+}
