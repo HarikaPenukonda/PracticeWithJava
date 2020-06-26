7f4385a0391716b3a9827d91223bd6e057394d79
harika penukonda <penukondaharika@gmail.com>
2020-06-26 11:21:01 -0500

    Update DecimalToOctal.java

--- PracticeWithJava/src/com/penukondah1/Coding/DecimalToOctal.java
+++ PracticeWithJava/src/com/penukondah1/Coding/DecimalToOctal.java
@@ -1,10 +1,18 @@
 package com.penukondah1.Coding;
 
+import java.util.Scanner;
+
 public class DecimalToOctal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the decimal number : ");
+		int num = sc.nextInt();
+		
+		String octalNumber = Integer.toOctalString(num); //predefined method  - Integer.toOctalString(int num)
+		System.out.println("Conversion of Decimal to Octal = " +octalNumber);
 	}
 
 }
