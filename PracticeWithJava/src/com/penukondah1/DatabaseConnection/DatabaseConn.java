2ff72a10c0839f0cb62fa3d76a71a7e7982795b7
harika penukonda <penukondaharika@gmail.com>
2020-06-29 11:07:13 -0500

    Update DatabaseConn.java

--- PracticeWithJava/src/com/penukondah1/DatabaseConnection/DatabaseConn.java
+++ PracticeWithJava/src/com/penukondah1/DatabaseConnection/DatabaseConn.java
@@ -2,6 +2,11 @@ package com.penukondah1.DatabaseConnection;
 
 import java.sql.*;
 
+//import javax.naming.Context;
+//import javax.naming.InitialContext;
+//import javax.naming.NamingException;
+//import javax.sql.DataSource;
+
 //Java DataBase Connectivity
 
 /* 1. import --> java.sql 
@@ -15,7 +20,7 @@ import java.sql.*;
 
 public class DatabaseConn {
 	
-	public static void main (String [] args) throws Exception
+	public static void main (String [] args) throws SQLException, ClassNotFoundException
 	{
 		//String url = "jdbc:mysql://localhost:3306/HarikaDB";
 		String url = "jdbc:mysql://localhost:3306/HarikaDB?serverTimezone=UTC";
@@ -23,8 +28,10 @@ public class DatabaseConn {
 		String password = "rootuser";
 		String query = "select sname from student where rollno=2";
 		
+		
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection(url,username,password);
+		
 		Statement stmt = con.createStatement();
 		ResultSet rs = stmt.executeQuery(query);
 		rs.next();
@@ -34,7 +41,14 @@ public class DatabaseConn {
 		
 		stmt.close();
 		con.close();
+		
+		
+		//datasourceName  jdbc/dsName,jdbc/java:dsName
+//		Context ct = new InitialContext();
+//		DataSource ds = (DataSource) ct.lookup("jdbc/dsName");
+//		Connection con = ds.getConnection();
+		
+		
 	}
-
 
 }
