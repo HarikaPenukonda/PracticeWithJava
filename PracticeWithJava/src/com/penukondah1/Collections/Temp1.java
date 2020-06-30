d63cc2450be18237c4cbb8fba64ad40ab2c881eb
harika penukonda <penukondaharika@gmail.com>
2020-06-30 14:38:03 -0500

    Create Temp1.java

---
+++ PracticeWithJava/src/com/penukondah1/Collections/Temp1.java
@@ -0,0 +1,22 @@
+package com.penukondah1.Collections;
+
+public class Temp1 {
+	
+	int i;
+	
+	Temp1(int i)
+	{
+		this.i = i;
+	}
+	
+	public int hashCode()
+	{
+		return i;
+	}
+	
+	public String toString()
+	{
+		return i + "";
+	}
+
+}
