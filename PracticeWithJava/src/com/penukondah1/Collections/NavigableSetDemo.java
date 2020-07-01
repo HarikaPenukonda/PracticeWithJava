9e9e986d7848e89415ced3e34772871bf4def44c
harika penukonda <penukondaharika@gmail.com>
2020-07-01 14:04:02 -0500

    Update NavigableSetDemo.java

--- PracticeWithJava/src/com/penukondah1/Collections/NavigableSetDemo.java
+++ PracticeWithJava/src/com/penukondah1/Collections/NavigableSetDemo.java
@@ -22,19 +22,19 @@ public class NavigableSetDemo {
 		
 		System.out.println(t); // [100, 101, 102, 103, 104, 105, 106, 107, 108]
 		
-		System.out.println(t.ceiling(105)); // 105
+		System.out.println(t.ceiling(105)); // 105  returns lowest element [108,107,106,105] >=105
 		
-		System.out.println(t.higher(105)); // 106
+		System.out.println(t.higher(105)); // 106 returns lowest element [108,107,106,105]>105
 		
-		System.out.println(t.floor(105)); // 105
+		System.out.println(t.floor(105)); // 105 returns highest element [101,102,103,104,105] <=105
 		
-		System.out.println(t.lower(105)); //104
+		System.out.println(t.lower(105)); //104 returns highest element [101,102,103,104]< 105
 		
 		System.out.println(t.descendingSet()); // [108, 107, 106, 105, 104, 103, 102, 101, 100]
 		
-		System.out.println(t.pollFirst()); // 100
+		System.out.println(t.pollFirst()); // 100 remove and returns first element
 		
-		System.out.println(t.pollLast()); //108
+		System.out.println(t.pollLast()); //108 remove and return last element
 		
 		System.out.println(t); // [101, 102, 103, 104, 105, 106, 107]
 
