983fff43aaaeab6dbac093c255edf6b2844b4e53
harika penukonda <penukondaharika@gmail.com>
2020-07-02 11:09:40 -0500

    Create AverageOfNumbers.java

---
+++ PracticeWithJava/src/com/penukondah1/Coding/AverageOfNumbers.java
@@ -0,0 +1,15 @@
+package com.penukondah1.Coding;
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+
+	}
+
+}
