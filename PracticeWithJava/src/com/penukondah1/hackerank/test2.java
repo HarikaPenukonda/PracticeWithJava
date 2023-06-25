535f7a61311f43d97d63aed84aa2e8566250450c
harika penukonda <penukondaharika@gmail.com>
2023-06-25 15:04:36 -0500

    Create test2.java

---
+++ PracticeWithJava/src/com/penukondah1/hackerank/test2.java
@@ -0,0 +1,15 @@
+package com.penukondah1.hackerank;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String message = "Hello";
+		for(int i=0;i<message.length();i++) {
+			System.out.println(message.charAt(i+1));
+		}
+
+	}
+
+}
