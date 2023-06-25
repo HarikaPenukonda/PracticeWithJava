588bd1fc9bfd13b59402e5814dd91343eccccd4d
harika penukonda <penukondaharika@gmail.com>
2023-06-25 15:04:46 -0500

    Create Test107.java

---
+++ PracticeWithJava/src/Test107.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class Test107 {
+	
+//	int merger(int[] a,int[] b) {
+//		int[] result = new int[a.length + b.length];
+//		for(int i=0;i<a.length;i++) {
+//			result[2*i] = a[i];
+//			result[(2*i)+1] = b[i];
+//		}
+//		return result;
+//	}
+	
+//	public boolean isSpecial(String text) {
+//		String tempText = alterText(text);
+//		return text.equals(tempText);
+//	}
+//	
+//	private String alterText(String inputText) {
+//		// TODO Auto-generated method stub
+//		if(inputText == null || inputText.isEmpty()) {
+//			return inputText;
+//		}
+//		return inputText.charAt(inputText.length()-1)+alterText(inputText.substring(0,inputText.length()-1));
+//		
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
