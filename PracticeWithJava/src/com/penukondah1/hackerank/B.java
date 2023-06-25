b01ca49be332f04ea271934e06285fa752be60e2
harika penukonda <penukondaharika@gmail.com>
2023-06-25 15:03:57 -0500

    Create B.java

---
+++ PracticeWithJava/src/com/penukondah1/hackerank/B.java
@@ -0,0 +1,14 @@
+package com.penukondah1.hackerank;
+
+public class B {
+	
+	class B1 extends B {}
+	
+	class B2 extends B {}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
