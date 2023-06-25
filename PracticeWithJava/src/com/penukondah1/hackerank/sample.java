04233f3fcd7f1f9a235f891c5c54bdf35224c583
harika penukonda <penukondaharika@gmail.com>
2023-06-25 15:04:24 -0500

    Create sample.java

---
+++ PracticeWithJava/src/com/penukondah1/hackerank/sample.java
@@ -0,0 +1,17 @@
+package com.penukondah1.hackerank;
+
+public class sample {
+	
+	public static void main(String[] args) {
+		
+		String message = "Hello world!";
+		String newMessage = message.substring(6,12) + message.substring(12,6);
+		System.out.println(newMessage);
+		
+		
+		
+		
+		
+	}
+
+}
